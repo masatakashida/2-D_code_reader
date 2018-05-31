e2a7585cfde59bfe5a4d76f36fd0d36a099d883b
masataka <ar.tec.masa@gmail.com>
2018-06-01 08:30:01 +0900

    一旦コピー

---
+++ arMaker.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cv2
+
+aruco = cv2.aruco
+# dictionaryを生成
+dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+
+def arGenerator():
+	fileName = "images/ar.png"
+	generator = aruco.drawMarker(dictionary, 0, 100)
+	cv2.imwrite(fileName, generator)
+
+	img = cv2.imread(fileName)
+	cv2.imshow('ArMaker', img)
+	cv2.waitKey(0)
+
+arGenerator()
