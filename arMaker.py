5a483edef7481516a5ad5454bdf2a2c10544c433
masataka <ar.tec.masa@gmail.com>
2018-06-03 17:49:07 +0900

    不要な部分を削除

--- arMaker.py
+++ arMaker.py
@@ -12,9 +12,4 @@ def code_generator():
         generator = aruco.drawMarker(dictionary, i, 100)
         cv2.imwrite(fileName, generator)
 
-        # 生成される画像を確認したい時
-        #img = cv2.imread(fileName)
-        #cv2.imshow('image', img)
-        #cv2.waitKey(0)
-
 code_generator()
