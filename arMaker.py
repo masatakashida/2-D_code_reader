601d3b14e0120bd4ed62156365fa6652756825be
masataka <ar.tec.masa@gmail.com>
2018-06-03 17:45:47 +0900

    インデントを調整

--- arMaker.py
+++ arMaker.py
@@ -7,14 +7,14 @@ aruco = cv2.aruco
 dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 
 def arGenerator():
-	for i in range(20):
-		fileName = "images/" + str(i) + ".png"
-		generator = aruco.drawMarker(dictionary, i, 100)
-		cv2.imwrite(fileName, generator)
+    for i in range(20):
+        fileName = "images/" + str(i) + ".png"
+        generator = aruco.drawMarker(dictionary, i, 100)
+        cv2.imwrite(fileName, generator)
 
-		# 生成される画像を確認したい時
-		#img = cv2.imread(fileName)
-		#cv2.imshow('image', img)
-		#cv2.waitKey(0)
+        # 生成される画像を確認したい時
+        #img = cv2.imread(fileName)
+        #cv2.imshow('image', img)
+        #cv2.waitKey(0)
 
 arGenerator()
