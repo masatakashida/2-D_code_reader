25aecfd3a03c275640d73cb363b9f629d12b9df9
masataka <ar.tec.masa@gmail.com>
2018-06-03 19:36:18 +0900

    fix

--- codeReader.py
+++ codeReader.py
@@ -13,18 +13,13 @@ def code_reader_by_camera():
         ret, frame = cap.read()
         height, width = frame.shape[:2]
         img = cv2.resize(frame,(int(width),int(height)))
-        # マーカを検出
         corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
-        # 検出したマーカーに描画する
-        aruco.drawDetectedMarkers(img, corners, ids, (0,255,0))
-        # 番号をターミナルに表示し続ける
         print(ids)
-        ## ToDo: 番号が配列の配列に収まっているので取り出す。（番号が認識されていないときは、Noneなので、単純に[0][0]で取り出してもエラーがおきるので注意）
+        # 二次元コードに描画する
+        aruco.drawDetectedMarkers(img, corners, ids, (0,255,0))
         cv2.imshow('drawDetectedMarkers', img)
-        # キーボード入力の受付
         cv2.waitKey(1)
 
-    # ビデオキャプチャーのメモリ解放
     cap.release()
     cv2.destroyAllWindows()
 
