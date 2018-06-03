b76b1975986463dadada85ff3616c7866b941a44
masataka <ar.tec.masa@gmail.com>
2018-06-03 19:57:25 +0900

    数字が配列から取り出されている状態で、表示されるように変更した

--- codeReader.py
+++ codeReader.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+import numpy
 
 aruco = cv2.aruco
 dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
@@ -14,13 +15,16 @@ def code_reader_by_camera():
         height, width = frame.shape[:2]
         img = cv2.resize(frame,(int(width),int(height)))
         corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
-        print(ids)
+        if isinstance(ids, numpy.ndarray) == True:
+            print(ids[0][0])
+        else:
+            print(ids)
         # 二次元コードに描画する
         #aruco.drawDetectedMarkers(img, corners, ids, (0,255,0))
         #cv2.imshow('drawDetectedMarkers', img)
-        cv2.waitKey(1)
+        cv2.waitKey(0)
 
     cap.release()
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
 
 code_reader_by_camera()
